ff0800695d83ebca43cc765f9d7ffe3c17b15255
Tyler Rutherford <rutherford87@gmail.com>
2021-03-25 23:21:08 -0500

    added example1.png and udpated readme.md. script is now a working file on a deployed URL.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # UniqPassword
+Contents of repo:
+* index.html
+* style.css
+* script.js
+* README.md
+
+This is my secure password generator, or UniqPassword.
+
+The user has the option to choose the length of their password, within 8 and 128 characters, and if it will contain at least one or up to all of the following characters:
+UpperCase Letters
+LowerCase Letters
+Numbers 0-9
+Special characters
+
+The repo can be found here: 
+[TPR_GitHub](https://github.com/rutherford87/UniqPassword)
+
+The website can be viewed here: [PROfolio by TRutherford](https://rutherford87.github.io/UniqPassword/)
+
+A sample password along with an example of how the prompts work is here:[UniqPassword](.Example1.PNG)
+
+
+Thanks for visiting!!
--- script.js
+++ script.js
@@ -1,56 +1,64 @@
-// Assignment Code
+// Assignment Code - generateBtn is a variable used to tie to the button on the HTML
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Start function to write password to the #password input
 function writePassword() {
-    function generatePassword ()
+    //function generatePassword (){
+    //Establish variables. Arrays 1-4 are initial options that will be concatenated together based on prompts
     var passwordText = document.querySelector("#password");
+  var array1 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+  //write quickly an uppercase array, from google. 
+  var array2 = array1.map(function(x) {return x.toUpperCase(); });
+  var array3 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var array4 = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+  var passwordLength = window.prompt('Password length?');
+//Kicks back to top of function if length criteria is not met
+    if (passwordLength <= 8 || passwordLength >=128 ) {
+    alert("Please enter value between 8 and 128");
+    writePassword ();
+}       else {
 
-  // stu code going forward
-  var input1 = window.prompt('Password length??');
-  var array1 = ['a', 'b', 'c', 'd', 'e'];
-  var array2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   
-//combine arrays based on prompts
-var alphaConf = confirm("Alpha?");
-var numConf = confirm("Numeric?");
-if (alphaConf == true && numConf == true) {
-    var array3 = array1.concat(array2);
- } else if (alphaConf == false && numConf == true){
-        var array3 = array2;
-    }   else if (alphaConf == true && numConf == false){
-            var array3 = array1;
-        }    else {
-                window.prompt('Refresh');            
-        }
-    
-    console.log(`this is my final array${array3}`);
-var pw = new Array(input1);
+//give user selection for lower case, upper case alpha, numbers, and special chars in the password
+var alphaLowerConf = confirm("Include lower case letters?");
+var alphaUpperConf = confirm("Include upper case letters?");
+var numConf = confirm("Include numbers?");
+var specConf = confirm("Include special characters?");
 
-console.log(array1.length);
+//declare initial empty final array. Combine arrays based on prompts. Could remove ==true later to save space.
+//if all prompts were false, restart at the top of the function.
+var finalArray = [];
+if (numConf){
+    finalArray = finalArray.concat(array3);}
+if (alphaLowerConf) {
+    finalArray = finalArray.concat(array1);}
+if (alphaUpperConf){
+    finalArray = finalArray.concat(array2);} 
+if (specConf){
+    finalArray = finalArray.concat(array4);} 
+if (numConf==false && alphaLowerConf==false && alphaUpperConf==false && specConf ==false){
+    alert("Please select at least one character type.")
+    writePassword();
+} else {
 
+    //check that an appropriate array was built
+    console.log(`this is my final array${finalArray}`);
+var pw = new Array(passwordLength);
 
-for (i=0; i<=input1; i++){
-    var rand = Math.floor(Math.random() * array3.length);
-    console.log(rand);
-pw[i] = array3[rand];
+for (i=0; i<=passwordLength; i++){
+    var rand = Math.floor(Math.random() * finalArray.length);
+pw[i] = finalArray[rand];
+}
+//reducing array down to a string without commas for printing to screen
+var pwStringA=pw.join(",");
+var pwStringB=pwStringA.replaceAll(",","");
+var finalPW = document.querySelector('#password');
+finalPW.textContent = pwStringB;
 }
 }
-
-
-  //return to instructor code
- // passwordText.value = pw;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
-// for restarting loop if not given proper values
-// at the very end of prompting, if all 4 booleans were false, restart
-// function generatePassword (){
-//     var passwordLength =prompt("Confirm Password Length Between 8-128 Characters.");
-//       if (passwordLength <= 8 || passwordLength >=128 ) {
-//         alert("Please enter value between 8 and 128");
-//         generatePassword ();
-//       } else {
-  
